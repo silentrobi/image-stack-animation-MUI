ff11b4836accbb9ecc039270adc6c199217c4b60
Mohammad Abu Musa Rabiul <mohammad.rabiul@tokeninc.com>
2021-07-30 14:26:52 +0300

    added image animation

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App
+# Image Stack animatio - Material-UI
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/components/Section1/index.js
+++ src/components/Section1/index.js
@@ -5,11 +5,10 @@ import React from 'react'
 import img1 from '../../images/screen_light_1.png';
 import img2 from '../../images/screen_light_2.png';
 import img3 from '../../images/screen_light_3.png';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import classNames from 'classnames';
 const useStyles = makeStyles((theme) => ({
     containerItem: {
-        paddingTop: theme.spacing(93),
+        paddingTop: theme.spacing(43),
         paddingBottom: theme.spacing(43),
         overflowX: 'hidden'
     },
@@ -23,86 +22,50 @@ const useStyles = makeStyles((theme) => ({
         paddingRight: 3,
         backgroundColor: 'rgb(223, 227, 232)',
         zIndex: 10,
-        transform: 'translate(-50% , 40px) skew(0deg, 8deg)',
-        borderRadius: 8,
-        '&:nth-child(2)': {
-            zIndex: 5,
-            transform: 'translate(0px, 0px) skew(0deg, 8deg)'
-        },
-        '&:nth-child(3)': {
-            zIndex: 1,
-            transform: 'translate(50%, -40px) skew(0deg, 8deg)',
-        }
+        transform: 'skew(0deg, 8deg)',
+        borderRadius: 8
     },
     imageContainer: {
         display: 'flex',
         flexDirection: 'row',
         flexGrow: 0,
-        // paddingLeft: '28%',
-        // [theme.breakpoints.up('md')]: {
-        //     paddingLeft: '35%',
-        // },
         position: 'relative',
         height: '100vh',
         alignItems: 'center',
         justifyContent: 'center'
     },
-    '@keyframe fadeInLeft': {
+    '@keyframes fadeInLeft': {
         '0%': {
             opacity: 0,
-            transform: 'translate(0% , 40px)'
+            transform: 'translate(0% , 40px) skew(0deg, 8deg)'
         },
         '100%': {
             opacity: 1,
-            transform: 'translate(-50%, 40px)'
+            transform: 'translate(-50%, 40px) skew(0deg, 8deg)'
         }
     },
-    '@-webkit-keyframes fadeInLeft': {
+    '@keyframes fadeInRight': {
         '0%': {
             opacity: 0,
-            '-webkit-transform': 'translate(0% , 40px)'
+            transform: 'translate(0% , -40px) skew(0deg, 8deg)'
         },
         '100%': {
             opacity: 1,
-            '-webkit-transform': 'translate(-50%, 40px)'
-        }
-    },
-    '@keyframe fadeInRight': {
-        '0%': {
-            opacity: 0,
-            transform: 'translate(0% , -40px)'
-        },
-        '100%': {
-            opacity: 1,
-            transform: 'translate(50%, -40px)'
-        }
-    },
-    '@-webkit-keyframes fadeInRight': {
-        '0%': {
-            opacity: 0,
-            '-webkit-transform': 'translate(0% , -40px)'
-        },
-        '100%': {
-            opacity: 1,
-            '-webkit-transform': 'translate(50%, -40px)'
+            transform: 'translate(50%, -40px) skew(0deg, 8deg)'         
         }
     },
     fadeInLeft: {
-        '-webkit-animation-name': 'fadeInLeft',
-        'animationName': 'fadeInLeft'
+        animation: `$fadeInLeft 3000ms ${theme.transitions.easing.easeInOut}`,
+        animationFillMode: 'both'
     },
     fadeInRight: {
-        '-webkit-animation-name': 'fadeInRight',
-        'animationName': 'fadeInRight'
+        animation: `$fadeInRight 3000ms ${theme.transitions.easing.easeInOut}`,
+        animationFillMode: 'both'
     }
 }));
 
 const Section1 = () => {
     const classes = useStyles();
-    useEffect(() => {
-        Aos.init({ once: true, duration: 2000, mirror: true })
-    }, []);
-
     return (
         <Grid container>
             <Grid item xs={12} className={classes.containerItem}>
@@ -111,12 +74,14 @@ const Section1 = () => {
                         //data-aos='fade-left'
                         component="img"
                         src={img1}
-                        className={classes.image}
+                        style={{ zIndex: 10}}
+                        className={classNames(classes.image, classes.fadeInLeft)}
                         alt='img1'
                     />
                     <Box
                         component="img"
                         src={img2}
+                        style={{ zIndex: 5, transform: 'transtalate(0px, 0px)'}}
                         className={classes.image}
                         alt='img2'
                     />
@@ -124,13 +89,14 @@ const Section1 = () => {
                         //data-aos='fade-right'
                         component="img"
                         src={img3}
-                        className={classes.image}
+                        style={{ zIndex: 1}}
+                        className={classNames(classes.image, classes.fadeInRight)}
                         alt='img3'
                     />
                 </Box>
             </Grid>
         </Grid >
-    )
+    );
 }
 
 export default Section1;
